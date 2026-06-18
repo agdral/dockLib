a92d0e7093a6d5f9a26613787b3ae60ce8adf278
agdral <ezequiel.escobar@proton.me>
2026-06-18 01:39:12 -0300

    fix home-manager root

--- user.nix
+++ user.nix
@@ -3,66 +3,64 @@
   pkgs,
   ...
 }: {
-  home-manager.users.podcal = {
-    imports = [
-      agenix.homeManagerModules.default
+  imports = [
+    agenix.homeManagerModules.default
+  ];
+  home = {
+    stateVersion = "26.05";
+
+    sessionVariables = {
+      UMASK = "007";
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+    };
+
+    sessionPath = [
+      "$HOME/.local/bin"
     ];
-    home = {
-      stateVersion = "26.05";
+  };
 
-      sessionVariables = {
-        UMASK = "007";
-        EDITOR = "nvim";
-        VISUAL = "nvim";
-      };
-
-      sessionPath = [
-        "$HOME/.local/bin"
-      ];
-    };
-
-    programs = {
-      bash = {
-        enable = true;
-        initExtra = "umask 0007";
-        profileExtra = ''
-          dbus-update-activation-environment --systemd --all
-          systemctl --user start podman-init.target
-        '';
-      };
-      fish = {
-        enable = true;
-        shellInit = "umask 0007";
-      };
-    };
-
-    services.podman = {
+  programs = {
+    bash = {
       enable = true;
-      networks = {
-        proxy_network = {
-          driver = "bridge";
-          subnet = "10.100.0.0/24";
-          gateway = "10.100.0.1";
-          extraPodmanArgs = ["--dns-enable=true"];
-        };
+      initExtra = "umask 0007";
+      profileExtra = ''
+        dbus-update-activation-environment --systemd --all
+        systemctl --user start podman-init.target
+      '';
+    };
+    fish = {
+      enable = true;
+      shellInit = "umask 0007";
+    };
+  };
+
+  services.podman = {
+    enable = true;
+    networks = {
+      proxy_network = {
+        driver = "bridge";
+        subnet = "10.100.0.0/24";
+        gateway = "10.100.0.1";
+        extraPodmanArgs = ["--dns-enable=true"];
       };
     };
+  };
 
-    systemd.user.sessionVariables = {
-      XDG_RUNTIME_DIR = "/run/user/1010";
-      DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/1010/bus";
-    };
+  systemd.user.sessionVariables = {
+    XDG_RUNTIME_DIR = "/run/user/1010";
+    DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/1010/bus";
+  };
 
-    home.packages = with pkgs; [
-      podman
-      podman-compose
-    ];
+  home.packages = with pkgs; [
+    podman
+    podman-compose
+  ];
 
-    systemd.user.targets.podman-init = {
-      Unit = {
-        Description = "Podman initialization";
-        After = ["default.target"];
-      };
+  systemd.user.targets.podman-init = {
+    Unit = {
+      Description = "Podman initialization";
+      After = ["default.target"];
     };
   };
 }
